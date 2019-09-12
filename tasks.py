a7872320917fcb76d2507e9b1b9031b0b5091259
Anders Goncalves da Silva <andersgs@gmail.com>
2019-09-12 10:32:46 -0700

    Implement automation of pypi deployment

--- tasks.py
+++ tasks.py
@@ -6,18 +6,46 @@ import invoke
 
 
 @invoke.task
-def deploy_patch(ctx):
+def clean(ctx):
     """
-    Automate deployment
-    rm -rf build/* dist/*
-    bumpversion patch --verbose
-    python3 setup.py sdist bdist_wheel
-    twine upload dist/*
-    git push --tags
+    Clean up build and dist before building submittion to pypi
     """
     ctx.run("rm -rf build/* dist/*")
+
+
+@invoke.task
+def check_for_unstaged_changes(ctx):
+    """
+    If unstaged changes raise an error
+    """
+    try:
+        ctx.run("git diff-index --quiet HEAD")
+    except invoke.exceptions.UnexpectedExit as error:
+        print(("ERROR: There are unstaged changes."))
+        raise error
+    except Exception as error:
+        raise error
+
+
+@invoke.task(pre=[check_for_unstaged_changes, clean])
+def deploy_patch(ctx):
+    """
+    Automate deployment to pypi
+    """
     ctx.run("bumpversion patch --verbose")
     ctx.run("python3 setup.py sdist bdist_wheel")
     ctx.run("twine check dist/*")
     ctx.run("twine upload dist/*")
+    ctx.run("git push --tags")
+
+
+@invoke.task(pre=[check_for_unstaged_changes, clean])
+def first_deploy(ctx):
+    """
+    First deployment to pypi
+    """
+    ctx.run("python3 setup.py sdist bdist_wheel")
+    ctx.run("twine check dist/*")
+    ctx.run("twine upload dist/*")
+    ctx.run("git tag 'v0.1.0'")
     ctx.run("git push --tags")
